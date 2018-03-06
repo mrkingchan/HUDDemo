d8c5952b1c0df2ce8a2d1d45e79bd0907bc5b11e
CherryIce <a13530836164@163.com>
2018-03-06 15:41:33 +0800

    mark 解释说明 hud不阻塞主线程 可以进行其他操作

--- HUDDemo/NextVC.m
+++ HUDDemo/NextVC.m
@@ -41,7 +41,7 @@
 }
 
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    iToastLoading;
+    iToastLoading;   //HUD不阻塞主线程的 需要在hud的初始那里禁止userInterface = NO
     [self loadData];
     _showErrorMessage.hidden = YES;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
